1961638e1028517831e42400d2b0efd0d8ebf8df
Vladimir_Shmalko <Vladimir_Shmalko@epam.com>
2023-03-31 15:10:41 +0200

    rename packages

---
+++ department-service/src/main/java/com/svs/services/department/DepartmentApplication.java
@@ -0,0 +1,54 @@
+package com.svs.services.department;
+
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import com.svs.services.department.model.Department;
+import com.svs.services.department.repository.DepartmentMongoRepository;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableSwagger2
+public class DepartmentApplication implements CommandLineRunner {
+
+  @Autowired
+  DepartmentMongoRepository repository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(DepartmentApplication.class, args);
+  }
+
+  @Bean
+  public Docket swaggerPersonApi10() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.svs.services.department.controller"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(new ApiInfoBuilder().version("1.0").title("Department API")
+            .description("Documentation Department API v1.0").build());
+  }
+
+  @Override
+  public void run(String... args)  {
+    repository.deleteAll();
+    repository.save(new Department(1L, 1L, "Operations", Collections.emptyList()));
+    repository.save(new Department(2L, 1L, "Operations", Collections.emptyList()));
+    repository.save(new Department(3L, 2L, "Development", Collections.emptyList()));
+    repository.save(new Department(4L, 2L, "Operations", Collections.emptyList()));
+  }
+
+
+}
---
+++ department-service/src/main/java/com/svs/services/department/model/Department.java
@@ -0,0 +1,36 @@
+package com.svs.services.department.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@SuperBuilder
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Document(collection = "Department")
+public class Department {
+
+  private Long Id;
+  private Long organizationId;
+  private String name;
+  private List<Employee> employees = new ArrayList<>();
+
+  public void setEmployees(List<Employee> employees) {
+    this.employees = employees;
+  }
+
+  @Override
+  public String toString() {
+    return "Department [id=" + ", organizationId=" + organizationId + ", name=" + name + "]";
+  }
+
+}
